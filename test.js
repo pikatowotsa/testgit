f4a9a5f2819cde934e133aec1fcb0e86e092ad0a
Pikato Wotsa <pikato@gmail.com>
2020-09-15 14:16:27 +0530

    view added

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 //A test program in javascript
 var j = 100;
 var k = document.getElementById('testid');
+
+var m = function () {
+    s = "hello";
+    return s;
+}
+console.log(m);
 console.log(j);
 
 
